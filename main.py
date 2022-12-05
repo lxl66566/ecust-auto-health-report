d9db2c5048bc2ce53dd52a83d04ec6f04c21f140
lxl66566 <18259734087@163.com>
2022-12-05 18:27:01 +0800

    test

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from playwright.sync_api import Playwright, sync_playwright
 from playwright._impl._api_types import TimeoutError as Terror
-import os
+import os,time
 
 
 def run(playwright: Playwright, stu_id, password) -> None:
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
-    # Open new page
     page = context.new_page()
     page.goto(
         "https://sso.ecust.edu.cn/authserver/login?service=https%3A%2F%2Fworkflow.ecust.edu.cn%2Fdefault%2Fwork%2Fuust%2Fzxxsmryb%2Fmrybcn.jsp")
@@ -15,14 +14,11 @@ def run(playwright: Playwright, stu_id, password) -> None:
     page.click("#password")
     page.fill("#password", password)
     page.locator('button').click()
-    # Click ins
     page.click("ins")
-    # Click text=下一步
     page.click("text=下一步")
+    time.sleep(0.5)
     page.click("label:has-text(\"健康\")")
-    # Click #radio_sfycxxwc42
     page.click("#radio_xrywz34")
-    # Click text=*行程码是否绿码： 是否 >> ins
     page.click("#radio_xcm5")
 
     for i in range(16,19):
@@ -31,17 +27,11 @@ def run(playwright: Playwright, stu_id, password) -> None:
         file_chooser = fc_info.value
         file_chooser.set_files('dead.png')
 
-    # print(os.path.abspath(os.path.dirname(__file__)) + os.sep + 'dead.png') # test
-
-    # 体温是否正常
     page.click("#radio_twsfzc9")
-    # <label for="sui-select-jkmsflm12" id="radio_jkmsflm13" class>…</label>
     page.click("#radio_jkmsflm13")
-    # <label for="sui-select-sfycxxwc41" id="radio_sfycxxwc42" class>…</label>
     page.click("#radio_sfycxxwc44")
-    # Click text=提交
+    time.sleep(0.5)
     page.click("text=提交")
-    # Click text=确定
     page.click("text=确定")
     try:
         page.click("text=确定")
